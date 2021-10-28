a5ec06ccd47502e75e1af98bbd4309da708ce3d0
Md.Antonin Islam <md.antonin6862@gmail.com>
2021-10-29 04:00:26 +0600

    added category & product table to model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    cover_image = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(null=True)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover_image = models.CharField(max_length=255)
+    availability = models.SmallIntegerField(default=1)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
