85ec24726ff6ed3edfe7f83fc35ff7002e22e4bf
Md.Antonin Islam <md.antonin6862@gmail.com>
2021-11-28 00:57:05 +0600

    frontend design updated

--- static/js/script.js
+++ static/js/script.js
@@ -8,25 +8,25 @@ menu.onclick = () =>{
 
 }
 
-window.onscroll = () =>{
+// window.onscroll = () =>{
 
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
+//   menu.classList.remove('fa-times');
+//   navbar.classList.remove('active');
 
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
+//   if(window.scrollY > 60){
+//     document.querySelector('#scroll-top').classList.add('active');
+//   }else{
+//     document.querySelector('#scroll-top').classList.remove('active');
+//   }
 
-}
+// }
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
+// function loader(){
+//   document.querySelector('.loader-container').classList.add('fade-out');
+// }
 
-function fadeOut(){
-  setInterval(loader, 3000);
-}
+// function fadeOut(){
+//   setInterval(loader, 3000);
+// }
 
-window.onload = fadeOut();
+// window.onload = fadeOut();
